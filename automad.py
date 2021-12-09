10ac25eda0c40e1f9b556ddc47c1a3b63735e0d0
Jan Hückelheim <jhueckelheim@anl.gov>
2021-12-09 12:25:54 -0600

    complete? example, but backward() is not called

--- automad.py
+++ automad.py
@@ -3,14 +3,18 @@ import torch
 class Fwd2Rev(torch.nn.Module):
     class __Func__(torch.autograd.Function):
         @staticmethod
-        def forward(ctx, input):
-            ctx.save_for_backward(input)
-            return input
+        def forward(ctx, fwdinput):
+            if(isinstance(fwdinput, list)):
+                fwdinput, input_d = fwdinput
+            ctx.save_for_backward(input_d)
+            return fwdinput
 
         @staticmethod
         def backward(ctx, grad_output):
-            input, = ctx.saved_tensors
-            return input
+            print("IEUWBHFILUWEBPIUGB")
+            input_d, = ctx.saved_tensors
+            grad_input = input_d*grad_output
+            return grad_input
 
     def __init__(self):
         super(Fwd2Rev, self).__init__()
@@ -21,37 +25,38 @@ class Fwd2Rev(torch.nn.Module):
 class Conv2d(torch.nn.Module):
     class __Func__(torch.autograd.Function):
         @staticmethod
-        def forward(ctx, input, weight, bias=None):
-            ctx.save_for_backward(input, weight, bias)
+        def forward(ctx, fwdinput, weight, bias=None):
+            if(isinstance(fwdinput, list)):
+                fwdinput, input_d = fwdinput
             with torch.no_grad():
-                ret = torch.nn.functional.conv2d(input, weight, bias)
-            return ret
+                # Primal Evaluation
+                ret = torch.nn.functional.conv2d(fwdinput, weight, bias)
+                # Derivative Propagation
+                n_directions = 0
+                if ctx.needs_input_grad[0]:
+                    return None, None, None
+                if ctx.needs_input_grad[1]:
+                    n_directions += 9
+                if bias is not None and ctx.needs_input_grad[2]:
+                    n_directions += 1
+                bias_d = torch.nn.Parameter(torch.zeros(n_directions), requires_grad=False)
+                weight_d = torch.nn.Parameter(torch.zeros(n_directions, 1, 3, 3), requires_grad=False)
+                if ctx.needs_input_grad[1]:
+                  for i in range(9):
+                      weight_d[i,0,i//3,i%3] = 1
+                if bias is not None and ctx.needs_input_grad[2]:
+                  bias_d[-1] = 1
+                ret_d = torch.nn.functional.conv2d(fwdinput, weight_d, bias_d)
+            return [ret, ret_d]
 
         @staticmethod
         def backward(ctx, grad_output):
-            input, weight, bias = ctx.saved_tensors
-
-            n_directions = 0
-            if ctx.needs_input_grad[0]:
-                return None, None, None
-            if ctx.needs_input_grad[1]:
-                n_directions += 9
-            if bias is not None and ctx.needs_input_grad[2]:
-                n_directions += 1
-            bias_d = torch.nn.Parameter(torch.zeros(n_directions), requires_grad=False)
-            weight_d = torch.nn.Parameter(torch.zeros(n_directions, 1, 3, 3), requires_grad=False)
-            if ctx.needs_input_grad[1]:
-              for i in range(9):
-                  weight_d[i,0,i//3,i%3] = 1
-            if bias is not None and ctx.needs_input_grad[2]:
-              bias_d[-1] = 1
-
-            ret_d = torch.nn.functional.conv2d(input, weight_d, bias_d)
-
-            grad = ret_d*grad_output
+            print("IEUWBHFILUWEBPIUGB")
             grad_input = None
-            grad_weight = grad[:,0:9,:,:].sum(dim=[0,2,3]).reshape(1,1,3,3) if ctx.needs_input_grad[1] else None
-            grad_bias = grad[:,-1,:,:].sum(dim=[0]) if bias is not None and ctx.needs_input_grad[2] else None
+            grad_weight = grad_output[:,0:9,:,:].sum(dim=[0,2,3]).reshape(1,1,3,3) if ctx.needs_input_grad[1] else None
+            grad_bias = grad_output[:,-1,:,:].sum(dim=[0]) if bias is not None and ctx.needs_input_grad[2] else None
+            print(grad_weight)
+            print(grad_bias)
             return grad_input, grad_weight, grad_bias
 
     def __init__(self, in_channels, out_channels, kernel_size, bias = True):
--- test.py
+++ test.py
@@ -5,13 +5,13 @@ class Net_AutoMAD(torch.nn.Module):
     def __init__(self):
         super(Net_AutoMAD, self).__init__()
         self.conv1 = automad.Conv2d(1, 1, 3)
-        self.conv2 = automad.Conv2d(1, 1, 3)
         self.f2r = automad.Fwd2Rev()
+        self.conv2 = torch.nn.Conv2d(1, 1, 3)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.conv2(x)
         x = self.f2r(x)
+        x = self.conv2(x)
         return x
 
 class Net_AutoGrad(torch.nn.Module):
@@ -27,6 +27,7 @@ class Net_AutoGrad(torch.nn.Module):
 
 n_batches = 7
 nninput = torch.randn(n_batches, 1, 16, 16)
+tgt = torch.randn(n_batches, 1, 12, 12)
 ##################
 # reverse mode AD
 ##################
@@ -34,11 +35,10 @@ netrev = Net_AutoGrad()
 netrev.conv1.bias = torch.nn.Parameter(torch.zeros(1), requires_grad=True)
 netrev.conv2.bias = torch.nn.Parameter(torch.zeros(1), requires_grad=True)
 netrev.zero_grad()
-out = netrev(nninput)
-loss = torch.nn.MSELoss(reduction='sum')
-tgt = torch.randn(n_batches, 1, 12, 12)
-l = loss(out, tgt)
-l.backward()
+outrev = netrev(nninput)
+lossrev = torch.nn.MSELoss(reduction='sum')
+lrev = lossrev(outrev, tgt)
+lrev.backward()
 print("""
 REVERSE AD
         """)
@@ -51,16 +51,16 @@ print(f"d_conv2_bias:\n{netrev.conv2.bias.grad}")
 # forward mode AD
 ##################
 netfwd = Net_AutoMAD()
-#primal setting, ensure it's the same as netrev
+#ensure we're using the same parameters and inputs as netrev
 netfwd.conv1.weight = netrev.conv1.weight
 netfwd.conv2.weight = netrev.conv2.weight
 netfwd.conv1.bias = netrev.conv1.bias
 netfwd.conv2.bias = netrev.conv2.bias
-netrev.zero_grad()
-out = netfwd(nninput)
-loss = torch.nn.MSELoss(reduction='sum')
-l = loss(out, tgt)
-l.backward()
+netfwd.zero_grad()
+outfwd = netfwd(nninput)
+lossfwd = torch.nn.MSELoss(reduction='sum')
+lfwd = lossfwd(outfwd, tgt)
+lfwd.backward()
 print("""
 MIXED MODE AD
         """)
