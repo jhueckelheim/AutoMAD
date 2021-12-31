implemented new style, in which F2R sums up across batches/image and passes only the computed gradient vector through backward(). some bug, currently works only for the last layer, or for a single batch.

automad.py
@@ -7,16 +7,20 @@ def channel2batch(x):
 class DualTensor(torch.Tensor):
     '''
     DualTensor objects store a primal tensor and its corresponding derivative
-    tensor. They otherwise inherit all functionality from torch.Tensor,
-    including size and requires_grad attributes.
-    TODO: This currently only supports 2d image data, but can be easily
-    adjusted to higher dimensions.
+    tensor. They otherwise inherit all functionality from torch.Tensor.
+    DualTensor also contains a "gradient" tensor, which is really just a vector.
+    During the forward pass it contains no useful information, and its length is
+    the number of all trainable parameters for all previous layers and the
+    current layer. The size of this vector is what is actually reported when the
+    size() function is called for the DualTensor. This is done so that PyTorch
+    will allow us to pass the gradient information to all layers during the
+    backward sweep.
     '''
     @staticmethod
-    def __new__(cls, xd, x, *args, **kwargs):
-        return super().__new__(cls, xd, *args, **kwargs)
+    def __new__(cls, xb, x, xd, *args, **kwargs):
+        return super().__new__(cls, xb, *args, **kwargs)
     
-    def __init__(self, xd, x):
+    def __init__(self, xb, x, xd):
         '''
         x is expected to be the primal data, xd is the corresponding derivative data.
         If xd contains multiple directional derivatives, they are expected to be
@@ -26,6 +30,7 @@ class DualTensor(torch.Tensor):
         '''
         super().__init__()
         self.x = x
+        self.xd = xd
         self.batches = x.size(0)  # number of true batches (in original network)
         self.channels = x.size(1) # number of true channels
         self.img_x = x.size(2)    # image size, in x dimension
@@ -62,8 +67,8 @@ class Fwd2Rev(torch.nn.Module):
     class __Func__(torch.autograd.Function):
         @staticmethod
         def forward(ctx, fwdinput):
-            input_p = torch.Tensor(fwdinput.x)
-            input_d = torch.Tensor(fwdinput)
+            input_p = fwdinput.x
+            input_d = fwdinput.xd
             ctx.save_for_backward(input_d)
             return input_p
 
@@ -72,8 +77,8 @@ class Fwd2Rev(torch.nn.Module):
             input_d, = ctx.saved_tensors
             input_d = input_d.unsqueeze(0).view(grad_output.size(0),input_d.size(0)//grad_output.size(0),input_d.size(1),input_d.size(2),input_d.size(3))
             print(f"mul input_d*grad_output, sizes {input_d.size()} {grad_output.size()}")
-            grad_input = input_d*grad_output.unsqueeze(2)
-            return grad_input.view(input_d.size(0)*input_d.size(1),input_d.size(2),input_d.size(3),input_d.size(4))
+            grad_input = (input_d*grad_output.unsqueeze(2)).sum(dim=[0,2,3,4])
+            return grad_input.view(input_d.size(1))
 
     def __init__(self):
         super(Fwd2Rev, self).__init__()
@@ -88,13 +93,14 @@ class Conv2d(torch.nn.Module):
             if(isinstance(fwdinput_dual, DualTensor)):
                 print("DUALTENSOR")
                 fwdinput = fwdinput_dual.x
-                fwdinput_d = torch.Tensor(fwdinput_dual)
+                fwdinput_d = fwdinput_dual.xd
+                num_dervs = fwdinput_dual.size(0)
             else:
                 print(f"SOMETHING ELSE: {type(fwdinput_dual)}")
                 fwdinput = fwdinput_dual
                 fwdinput_d = None
+                num_dervs = 0
             n_batches = fwdinput.size(0)
-            ctx.n_batches = n_batches
             with torch.no_grad():
                 if ctx.needs_input_grad[0]:
                     print(f"ctx cat fwdinput {fwdinput.size()}, fwdinput_d {fwdinput_d.size()}")
@@ -122,17 +128,13 @@ class Conv2d(torch.nn.Module):
                 print(f"ret_d cat ret_d1 {ret_d1.size()}, retd2 {ret_d2.size()}")
                 ret_d = torch.cat([ret_d1, channel2batch(ret_d2)], dim=0)
             ret_p.requires_grad = True
-            ret_dual = DualTensor(ret_d, ret_p)
+            ret_dual = DualTensor(torch.zeros(10+num_dervs), ret_p, ret_d)
             print(f"returning DualTensor({type(ret_d)} {type(ret)}) with sizes {ret_d.size()} {ret_p.size()}")
             return ret_dual
 
         @staticmethod
         def backward(ctx, grad_output):
             print(f"conv backw receiving grad_out with {grad_output.size()}")
-            n_batches = ctx.n_batches
-            # We create a view to distinguish true batches from batches that
-            # represent directional derivatives.
-            grad_output = grad_output.view(n_batches, grad_output.size(0)//n_batches, grad_output.size(1), grad_output.size(2), grad_output.size(3))
             # grad_input is the pass-through of gradient information for layers
             # that happened before us in the forward pass (and will happen after
             # this in the reverse pass).
@@ -140,12 +142,11 @@ class Conv2d(torch.nn.Module):
             # derivatives belong to the current layer. This layer has 10
             # trainable parameters, so everything except the last 10 directions
             # is passed into grad_input.
-            grad_input = grad_output[:,:-10,:,:,:]
-            grad_input = grad_input.reshape(grad_input.size(0)*grad_input.size(1), grad_input.size(2), grad_input.size(3), grad_input.size(4))
-            # grad_weight and grad_bias are the incoming grad_out, summed up
-            # over the true channels.
-            grad_weight = grad_output[:,-10:-1,:,:,:].sum(dim=[0,2,3,4]).reshape(1,1,3,3) if ctx.needs_input_grad[1] else None
-            grad_bias = grad_output[:,-1:,:,:,:].sum(dim=[0,2,3,4]) if ctx.needs_input_grad[2] else None
+            grad_input = grad_output[:-10]
+            # the rest is gradient info for the current layer. The weights are
+            # first, the bias is last.
+            grad_weight = grad_output[-10:-1].view(1,1,3,3)
+            grad_bias = grad_output[-1].view(1)
             return grad_input, grad_weight, grad_bias
 
     def __init__(self, in_channels, out_channels, kernel_size, bias = True):

test.py
@@ -5,13 +5,13 @@ class Net_AutoMAD(torch.nn.Module):
     def __init__(self):
         super(Net_AutoMAD, self).__init__()
         self.conv1 = automad.Conv2d(1, 1, 3)
-        self.conv2 = automad.Conv2d(1, 1, 3)
         self.f2r = automad.Fwd2Rev()
+        self.conv2 = torch.nn.Conv2d(1, 1, 3)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.conv2(x)
         x = self.f2r(x)
+        x = self.conv2(x)
         return x
 
 class Net_AutoGrad(torch.nn.Module):