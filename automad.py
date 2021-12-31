4adb20364fa0a51fa7562401a27a9cc3f0d7b2f2
Jan Hückelheim <jhueckelheim@anl.gov>
2021-12-30 21:11:39 -0600

    adding work-in-progress for implementation that uses back-propagation machinery to communicate gradients computed by F2R to all forward layers. this is probably a poor design choice and we will revert and ditch this approach for now.

--- automad.py
+++ automad.py
@@ -1,22 +1,59 @@
 import torch
 
+def channel2batch(x):
+    # reinterpret a [x,y,:,:] tensor as a [x*y,1,:,:] tensor
+    return x.view(x.size(1)*x.size(0),1,x.size(2),x.size(3))
+
 class DualTensor(torch.Tensor):
     '''
     DualTensor objects store a primal tensor and its corresponding derivative
     tensor. They otherwise inherit all functionality from torch.Tensor,
     including size and requires_grad attributes.
+    TODO: This currently only supports 2d image data, but can be easily
+    adjusted to higher dimensions.
     '''
     @staticmethod
     def __new__(cls, xd, x, *args, **kwargs):
         return super().__new__(cls, xd, *args, **kwargs)
     
     def __init__(self, xd, x):
+        '''
+        x is expected to be the primal data, xd is the corresponding derivative data.
+        If xd contains multiple directional derivatives, they are expected to be
+        stored as channels. The number of channels in xd is the product of the
+        number of channels in the primal data (the "true" channels) and the
+        number of directional derivatives.
+        '''
         super().__init__()
         self.x = x
+        self.batches = x.size(0)  # number of true batches (in original network)
+        self.channels = x.size(1) # number of true channels
+        self.img_x = x.size(2)    # image size, in x dimension
+        self.img_y = x.size(3)    # image size, in y dimension
+        self.nbdirs = xd.size(1) / x.size(1) # number of directional derivatives
+
+    def derivativesAsBatches(self):
+      '''
+      Return a view in which the directional derivatives are merged into the
+      batches. This can be useful if the same operation should be applied to all
+      directional derivatives across all true batches. (Where a "true" batch is
+      one that existed in the original network, to which we add additional
+      batches that are actually directional derivatives.)
+      '''
+      return self.view(self.batches*self.nbdirs, self.channels, self.img_x, self.img_y)
+
+    def unsqueezeDerivatives(self):
+      '''
+      Return a view in which the directional derivatives are a separate
+      dimension, just inside the batch dimension.
+      '''
+      ret = self.unsqueeze(0)
+      ret = ret.view(self.batches, self.nbdirs, self.channels, self.img_x, self.img_y)
+      return ret
 
 class Fwd2Rev(torch.nn.Module):
     '''
-    A neural network layer whose only purpose is to glue a forward-mode AD and
+    A neural network layer whose only purpose is to glue forward-mode AD and
     reverse-mode AD together. It will store forward-propagated derivatives
     during the forward sweep, and combine them with the reverse-propagated
     derivatives during the reverse sweep, immediately resulting in the gradient
@@ -33,8 +70,10 @@ class Fwd2Rev(torch.nn.Module):
         @staticmethod
         def backward(ctx, grad_output):
             input_d, = ctx.saved_tensors
-            grad_input = input_d*grad_output
-            return grad_input
+            input_d = input_d.unsqueeze(0).view(grad_output.size(0),input_d.size(0)//grad_output.size(0),input_d.size(1),input_d.size(2),input_d.size(3))
+            print(f"mul input_d*grad_output, sizes {input_d.size()} {grad_output.size()}")
+            grad_input = input_d*grad_output.unsqueeze(2)
+            return grad_input.view(input_d.size(0)*input_d.size(1),input_d.size(2),input_d.size(3),input_d.size(4))
 
     def __init__(self):
         super(Fwd2Rev, self).__init__()
@@ -45,16 +84,29 @@ class Fwd2Rev(torch.nn.Module):
 class Conv2d(torch.nn.Module):
     class __Func__(torch.autograd.Function):
         @staticmethod
-        def forward(ctx, fwdinput, weight, bias=None):
-            if(isinstance(fwdinput, list)):
-                fwdinput, input_d = fwdinput
+        def forward(ctx, fwdinput_dual, weight, bias=None):
+            if(isinstance(fwdinput_dual, DualTensor)):
+                print("DUALTENSOR")
+                fwdinput = fwdinput_dual.x
+                fwdinput_d = torch.Tensor(fwdinput_dual)
+            else:
+                print(f"SOMETHING ELSE: {type(fwdinput_dual)}")
+                fwdinput = fwdinput_dual
+                fwdinput_d = None
+            n_batches = fwdinput.size(0)
+            ctx.n_batches = n_batches
             with torch.no_grad():
-                # Primal Evaluation
-                ret = torch.nn.functional.conv2d(fwdinput, weight, bias)
+                if ctx.needs_input_grad[0]:
+                    print(f"ctx cat fwdinput {fwdinput.size()}, fwdinput_d {fwdinput_d.size()}")
+                    fwdinput_pd = torch.cat([fwdinput, fwdinput_d], dim=0)
+                else:
+                    fwdinput_pd = fwdinput
+                # Primal Evaluation (includes weight*fwdinput_d)
+                ret = torch.nn.functional.conv2d(fwdinput_pd, weight, bias)
+                ret_p = ret[0:n_batches,:,:,:]
+                ret_d1 = ret[n_batches:,:,:,:]
                 # Derivative Propagation
                 n_directions = 0
-                if ctx.needs_input_grad[0]:
-                    return None, None, None
                 if ctx.needs_input_grad[1]:
                     n_directions += 9
                 if bias is not None and ctx.needs_input_grad[2]:
@@ -66,16 +118,34 @@ class Conv2d(torch.nn.Module):
                       weight_d[i,0,i//3,i%3] = 1
                 if bias is not None and ctx.needs_input_grad[2]:
                   bias_d[-1] = 1
-                ret_d = torch.nn.functional.conv2d(fwdinput, weight_d, bias_d)
-            ret.requires_grad = True
-            ret_dual = DualTensor(ret_d, ret)
+                ret_d2 = torch.nn.functional.conv2d(fwdinput, weight_d, bias_d)
+                print(f"ret_d cat ret_d1 {ret_d1.size()}, retd2 {ret_d2.size()}")
+                ret_d = torch.cat([ret_d1, channel2batch(ret_d2)], dim=0)
+            ret_p.requires_grad = True
+            ret_dual = DualTensor(ret_d, ret_p)
+            print(f"returning DualTensor({type(ret_d)} {type(ret)}) with sizes {ret_d.size()} {ret_p.size()}")
             return ret_dual
 
         @staticmethod
         def backward(ctx, grad_output):
-            grad_input = None
-            grad_weight = grad_output[:,0:9,:,:].sum(dim=[0,2,3]).reshape(1,1,3,3) if ctx.needs_input_grad[1] else None
-            grad_bias = grad_output[:,-1,:,:].sum(dim=[0]) if ctx.needs_input_grad[2] else None
+            print(f"conv backw receiving grad_out with {grad_output.size()}")
+            n_batches = ctx.n_batches
+            # We create a view to distinguish true batches from batches that
+            # represent directional derivatives.
+            grad_output = grad_output.view(n_batches, grad_output.size(0)//n_batches, grad_output.size(1), grad_output.size(2), grad_output.size(3))
+            # grad_input is the pass-through of gradient information for layers
+            # that happened before us in the forward pass (and will happen after
+            # this in the reverse pass).
+            # The gradient tensor is organized such that the last directional
+            # derivatives belong to the current layer. This layer has 10
+            # trainable parameters, so everything except the last 10 directions
+            # is passed into grad_input.
+            grad_input = grad_output[:,:-10,:,:,:]
+            grad_input = grad_input.reshape(grad_input.size(0)*grad_input.size(1), grad_input.size(2), grad_input.size(3), grad_input.size(4))
+            # grad_weight and grad_bias are the incoming grad_out, summed up
+            # over the true channels.
+            grad_weight = grad_output[:,-10:-1,:,:,:].sum(dim=[0,2,3,4]).reshape(1,1,3,3) if ctx.needs_input_grad[1] else None
+            grad_bias = grad_output[:,-1:,:,:,:].sum(dim=[0,2,3,4]) if ctx.needs_input_grad[2] else None
             return grad_input, grad_weight, grad_bias
 
     def __init__(self, in_channels, out_channels, kernel_size, bias = True):
