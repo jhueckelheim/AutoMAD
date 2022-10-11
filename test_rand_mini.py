f6abe419090b9060e59f60ccae327028a419f41e
Jan Hückelheim <jhueckelheim@anl.gov>
2022-10-10 19:30:33 -0500

    added QR forward gradients

--- test_rand_mini.py
+++ test_rand_mini.py
@@ -51,12 +51,18 @@ def test_forward_reverse(n_runs):
     netfwd.linear.weight = torch.nn.Parameter(netrev.linear.weight)
     netfwd.linear.bias = torch.nn.Parameter(netrev.linear.bias)
 
+    ########################
+    # Conventional backprop
+    ########################
     outrev = netrev(nninput)
     lossrev = torch.nn.MSELoss(reduction='sum')
     lrev = lossrev(outrev, tgt)
     lrev.backward()
     all_rev = torch.cat([param.grad.flatten().clone() for param in netrev.parameters() if param.grad != None])
 
+    ########################
+    # Forward gradients
+    ########################
     all_fwd = None
     for i in range(n_runs):
         netfwd.zero_grad()
@@ -71,26 +77,43 @@ def test_forward_reverse(n_runs):
         all_fwd[i,:] = torch.cat([param.grad.flatten().clone() for param in netfwd.parameters() if param.grad != None])
     all_fwd = torch.mean(all_fwd, dim=0)
 
+    ########################
+    # Compare with dot prod
+    ########################
     all_fwd_2 = torch.zeros(n_runs, n_grads)
+    v = torch.normal(size=all_fwd_2.size(), mean=0.0, std=1.0)
     for i in range(n_runs):
-        v = torch.normal(size=all_fwd_2[i,:].size(), mean=0.0, std=1.0)
-        all_fwd_2[i,:] = sum(all_rev * v) * v
+        all_fwd_2[i,:] = sum(all_rev * v[i,:]) * v[i,:]
     all_fwd_2 = torch.mean(all_fwd_2, dim=0)
+
+    ########################
+    # Compare with QR
+    ########################
+    all_fwd_3 = torch.zeros(n_runs, n_grads)
+    v = torch.normal(size=all_fwd_3.size(), mean=0.0, std=1.0)
+    VQ, VR = torch.linalg.qr(v.transpose(0,1), mode='reduced')
+    VQ = -VQ.transpose(0,1)
+    for i in range(n_runs):
+        all_fwd_3[i,:] = sum(all_rev * VQ[i,:]) * VQ[i,:]
+    all_fwd_3 = torch.mean(all_fwd_3, dim=0)
 
     all_rev_n = all_rev/torch.norm(all_rev)
     all_fwd_n = all_fwd/torch.norm(all_fwd)
     all_fw2_n = all_fwd_2/torch.norm(all_fwd_2)
+    all_fw3_n = all_fwd_3/torch.norm(all_fwd_3)
 
     
 
-    angle = torch.arccos(torch.clip(torch.dot(all_rev_n, all_fwd_n), -1.0, 1.0))
-    angle = angle*180/torch.pi
-    print("Angle between true and approximate gradient after %d runs with %d params: %f degrees"%(n_runs,all_rev.size(0),angle))
+    angle1 = torch.arccos(torch.clip(torch.dot(all_rev_n, all_fwd_n), -1.0, 1.0))
+    angle1 = angle1*180/torch.pi
 
-    angle = torch.arccos(torch.clip(torch.dot(all_rev_n, all_fw2_n), -1.0, 1.0))
-    angle = angle*180/torch.pi
-    print("Angle between true and approximate gradient after %d runs with %d params: %f degrees"%(n_runs,all_rev.size(0),angle))
-    #assert angle < 30
+    angle2 = torch.arccos(torch.clip(torch.dot(all_rev_n, all_fw2_n), -1.0, 1.0))
+    angle2 = angle2*180/torch.pi
+
+    angle3 = torch.arccos(torch.clip(torch.dot(all_rev_n, all_fw3_n), -1.0, 1.0))
+    angle3 = angle3*180/torch.pi
+
+    print(f"Angle with {n_runs} runs and {all_rev.size(0)} params: fwd_grad {angle1}, check_grad {angle2}, QR_grad {angle3}")
 
 if __name__ == '__main__':
     n_runs = 1000
